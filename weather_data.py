8d613300129aaf2c64061294e61246e86c5dcf9d
blvadel <a.blyalova@chocotravel.com>
2024-04-26 23:34:19 +0500

    get current weather

--- weather_data.py
+++ weather_data.py
@@ -1,1 +1,18 @@
-print("Здесь будет код, с помошью которого будет будет получена информация о погоде")
+import requests
+import requests.exceptions
+
+
+def get_current_weather():
+    #http://api.weatherapi.com/v1/current.json?key=c70ccfb2c56143f5893175705242604&q=London&aqi=no
+    url = 'http://api.weatherapi.com/v1/current.json'
+    city =(input())
+    params = {'key': 'c70ccfb2c56143f5893175705242604',
+              'q' : city,
+              'aqi' : 'no'
+    }
+    r = requests.get(url, params)
+    result = r.json()
+    print(result)
+
+
+get_current_weather()
