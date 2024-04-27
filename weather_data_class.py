cc5059d2eb4784b0428f24475bd9dcf12360a36b
blvadel <a.blyalova@chocotravel.com>
2024-04-27 19:51:39 +0500

    updated weather data

--- weather_data_class.py
+++ weather_data_class.py
@@ -11,7 +11,7 @@ class weather_data():
 
 
     def get_current_weather_data(self):
-        url = "http://api.weatherapi.com/v1/forecast.json"
+        url = "http://api.weatherapi.com/v1/current.json"
         params = {'key': self.key,
                   'q': self.city
                   }
@@ -21,13 +21,13 @@ class weather_data():
         current_dict = json.loads(current)
         current_dict_beauty = json.dumps(current_dict, indent=4, ensure_ascii=False)
         print(f'Status code is {s_code}')
-        print(current_dict_beauty)
+        return current_dict
 
 
 
 
     def get_weather_forecast(self):
-        url = "http://api.weatherapi.com/v1/current.json"
+        url = "http://api.weatherapi.com/v1/forecast.json"
         params = {'key': self.key,
                   'q': self.city,
                   'days': self.days
@@ -36,9 +36,8 @@ class weather_data():
         s_code = r.status_code
         forecast = r.content
         forecast_dict = json.loads(forecast)
-        forecast_dict_beauty = json.dumps(forecast_dict, indent=4, ensure_ascii=False)
         print(f'Status code is {s_code}')
-        print(forecast_dict_beauty)
+        return forecast_dict
 
 
 
@@ -52,16 +51,9 @@ class weather_data():
         s_code = r.status_code
         future = r.content
         future_dict = json.loads(future)
-        future_dict_beauty = json.dumps(future_dict, indent=4, ensure_ascii=False)
         print(f'Status code is {s_code}')
-        print(future_dict_beauty)
+        return future_dict
 
-
-
-city_weather = weather_data("Almaty", 2,"2024-05-26")
-city_weather.get_current_weather_data()
-city_weather.get_weather_forecast()
-city_weather.get_future_forecast()
 
 
 
